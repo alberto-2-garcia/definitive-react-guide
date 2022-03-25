e1d5748bb58bb978a0e909d9e05afed8f1c61107
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-03-25 11:18:19 -0600

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react'
 import Layout from './components/Layout';
+import Navbar from './components/Navbar';
 import Title from './components/Title';
 import ShoppingCart from './containers/ShoppingCart';
 
 const App: FC = () => {
   return (
     <div>
+      <Navbar />
       <Layout>
         <Title> Tienda </Title>
         <ShoppingCart />
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import './styles.scss';
+
+const Navbar: FC = () => {
+  return (
+    <nav className='navbar'>
+      <p>Logo</p>
+      <p>Carro</p>
+    </nav>
+  )
+}
+
+export default Navbar;
