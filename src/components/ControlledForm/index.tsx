3b9015c53499c5070bf13f773532ba9de5d46f1a
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-03-27 17:34:26 -0600

    Added select to ControlledForm

--- src/components/ControlledForm/index.tsx
+++ src/components/ControlledForm/index.tsx
@@ -3,10 +3,11 @@ import React, { ChangeEvent, useState } from 'react'
 const ControlledForm = () => {
   const [formValues, setFormValues] = useState({
     input: '',
-    area: ''
+    area: '',
+    select: ''
   });
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
     const { name, value } = e.target
 
     setFormValues({
@@ -15,12 +16,17 @@ const ControlledForm = () => {
     });
   }
 
-  const { input, area } = formValues;
+  const { input, area, select } = formValues;
 
   return (
     <div>
       <input type='text' name='input' value={input} onChange={handleChange} />
       <textarea name="area" value={area} onChange={handleChange} ></textarea>
+      <select name='select' value={select} onChange={handleChange}>
+        <option value=''>--- Selecciona ---</option>
+        <option value='hola'>hola</option>
+        <option value='adios'>adios</option>
+      </select>
     </div>
   )
 }
