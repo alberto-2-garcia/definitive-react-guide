64ef3b6753e48fed98b29fac68cfd803aa572f70
alberto-2-garcia <alberto.2.garcia@atos.net>
2022-03-28 14:04:51 -0600

    Added simple validation to form

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,25 @@ import ControlledForm from './components/ControlledForm'
 import { useFormik } from 'formik'
 import Input from './components/Input'
 
+interface FormValues {
+  name: string;
+  lastname: string;
+  email: string;
+}
+
+const initialValues: FormValues = {
+  name: '',
+  lastname: '',
+  email: ''
+}
 
 function App() {
-  const { handleSubmit, handleChange, values } = useFormik({
-    initialValues: {
-      name: '',
-      lastname: '',
-      email: ''
+  const { handleSubmit, handleChange, values, errors } = useFormik({
+    initialValues,
+    validate: (values) => {
+      const errors: FormValues = { ...initialValues };
+      errors.name = 'Requerido';
+      return errors;
     },
     onSubmit: (values) => {
       console.log(values)
@@ -20,10 +32,12 @@ function App() {
   });
 
   const { name, lastname, email } = values;
+  const { name: errorName, lastname: errorLastname, email: errorEmail } = errors;
 
   return (
     <form onSubmit={handleSubmit}>
       <Input type='text' label='Nombre' onChange={handleChange} value={name} name='name'/>
+      {errorName && <p>{errorName}</p>}
       <Input type='text' label='Apellido' onChange={handleChange} value={lastname} name='lastname'/>
       <Input type='email' label='Correo' onChange={handleChange} value={email} name='email'/>
       <input type='submit' value='Enviar'/>
