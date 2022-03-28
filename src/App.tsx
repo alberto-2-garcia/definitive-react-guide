7083b70b9c7c678fb8bf8a435bdb26a32e8fcadc
alberto-2-garcia <alberto.2.garcia@atos.net>
2022-03-28 16:06:30 -0600

    Finished ImageSearch

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import logo from './logo.svg'
 // import './App.css'
 import UncontrolledForm from './components/UncontrolledForm'
 import ControlledForm from './components/ControlledForm'
+import ImageSearch from './containers/ImageSearch'
 
 function App() {
   return (
-    <ControlledForm />
+    <ImageSearch />
   )
 }
 
--- src/containers/ImageSearch/index.tsx
+++ src/containers/ImageSearch/index.tsx
@@ -1,8 +1,60 @@
-import React, { FC } from 'react'
+import { Field, Form, Formik } from 'formik';
+import { FC, useState } from 'react';
+
+type URLs = {
+  raw: string;
+  full: string;
+  regular: string;
+  small: string;
+  thumb: string;
+}
+
+type Links = {
+  html: string;
+}
+
+type PhotoType = {
+  id: string;
+  urls: URLs;
+  links: Links;
+  description: string;
+  alt_description: string;
+}
 
 const ImageSearch: FC = () => {
+  const [photos, setPhotos] = useState<PhotoType[]>([]);
+  const open = (url: string) => window.open(url)
   return (
-    <div>ImageSearch</div>
+    <div>
+      <div>
+        <Formik 
+          initialValues={{ search: '' }}
+          onSubmit={async values => {
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+              headers: {
+                'Authorization': 'Client-ID qvy2y7fRCE_mIZrDJ8fevnKO8Mm29ZYhiXQSRq70mOw'
+              }
+            })
+            const data = await response.json()
+            setPhotos(data.results)
+          }}
+        >
+          <Form>
+            <Field name='search' />
+          </Form>
+        </Formik>
+      </div>
+      <div className="container">
+        <div className="center">
+          {photos.map(photo => 
+          <article key={photo.id} onClick={() => open(photo.links.html)}>
+            <img src={photo.urls.regular} />
+            <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+          </article>
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
 
