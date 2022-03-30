ac8391fa219c60c007aaf295822400d7d6641e3c
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-03-30 14:46:52 -0600

    Used Image component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
+import Octoexpansion from '../public/octoexpansion.jpg';
 
 const Home: NextPage = () => {
   return (
@@ -8,6 +10,8 @@ const Home: NextPage = () => {
       <Link href='/about'>About</Link>
       <Link href='/proyectos'>Proyectos</Link>
       <Link href='/pokemones'>Pokemones</Link>
+      <Image src='/octoexpansion.jpg' width={200} height={100} />
+      <Image src={Octoexpansion} width={200} height={100} />
     </div>
   )
 }
