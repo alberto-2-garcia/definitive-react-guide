1a138ceae84278647a0b4ca66d018749e52efed5
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-03-30 15:26:26 -0600

    Added pokemons

--- pages/pokemones/index.tsx
+++ pages/pokemones/index.tsx
@@ -1,14 +1,43 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage, InferGetStaticPropsType} from 'next';
 import Link from 'next/link';
 
-const Pokemones: NextPage = () => {
+interface PokemonType {
+  name: string;
+  url: string;
+}
+interface PokemonAPIResponse {
+  results: PokemonType[];
+  count: number;
+  next: string | null;
+  previous: string | null;
+}
+
+const Pokemones: NextPage = ({ pokemones }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  // console.log(pokemones)
   return (
     <div>
       <h1>Pokemones</h1>
       <Link href='/'>Inicio</Link>
-      <Link href='/pokemones/pikachu'>Pikachu</Link>
+      <ul>
+        {(pokemones as PokemonAPIResponse).results.map(({ name, url }) => (
+          <li key={name}>
+            <Link href={`/pokemones/${name}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+  const data = await response.json() as PokemonAPIResponse;
+
+  return {
+    props: {
+      pokemones: data
+    }
+  }
 }
 
 export default Pokemones;
