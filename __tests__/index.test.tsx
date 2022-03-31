b29fb63f199813a79291dc2d62a7882ecbf25d31
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-03-31 10:59:41 -0600

    Added tests

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,15 @@
+describe('Index', () => {
+  describe('Component', () => {
+    it('se renderiza', () => {
+      expect(2 + 2).toBe(4);
+    })
+  })
+
+  describe('getStaticProps', () => {
+    it('suma 2 + 2', () => {
+      expect(2 + 2).toBe(4);
+    })
+  })
+});
+
+export {}
